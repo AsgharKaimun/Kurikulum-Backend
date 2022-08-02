90dcfdcc9b10969bd3cda0678a058dd31a8757eb
AsgharKaimun <asgharkaimun54@gmail.com>
2022-08-02 17:06:12 +0700

    Tugas Hari_1

---
+++ Hari_1/CreateVariable.php
@@ -0,0 +1,18 @@
+<?php
+
+$Nama = "Faturrahman"; // String => Karakter
+$Tinggi = 167; // Integer => Bilangan Bulat
+$Nilai = 83.5; // Float => Bilangan Desimal / Pecahan
+
+// Cara 1 Menampilkan Variabel Di Dalam String
+echo "Nama Saya : $Nama" . PHP_EOL;
+
+// Cara 2 Menampilkan Variabel Di Dalam String
+echo "Tinggi Badan : $Tinggi cm \n";
+
+//Cara 3 Menampilkan Variabel Di Dalam String
+echo "Kemampuan : ";
+echo "$Nilai Good";
+echo "\n";
+
+?>
---
+++ Hari_1/PenulisanCase.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+    Variabel $nama dan $NAMA adalah dua variabel yang berbeda
+    Jangan salah memanggil Variabel karena bisa menyebabkan eror  
+*/
+$nama = "Fathur \n"; 
+
+$NAMA = "Faturrahman";
+
+echo "Nama Saya = $nama";
+echo "Nama Saya = $NAMA" . PHP_EOL;
+
+?>
