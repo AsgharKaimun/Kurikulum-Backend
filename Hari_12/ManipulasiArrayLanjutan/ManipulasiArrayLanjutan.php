02ccc4db4495fdaded46bee7ec60d3aef8e62b6b
AsgharKaimun <asgharkaimun54@gmail.com>
2022-08-26 14:52:17 +0700

    Menambah Catatan

--- Hari_12/ManipulasiArrayLanjutan/ManipulasiArrayLanjutan.php
+++ Hari_12/ManipulasiArrayLanjutan/ManipulasiArrayLanjutan.php
@@ -1,2 +1,19 @@
 <?php
 
+$SOD = [
+    "Powerhouse" => "Marcus",
+    "Cardio Run" => "Ed",
+    "EagleEye" => "Maya",
+    "Operator" => "Lily",
+    "The Army" => "Erik Tan",
+    "The Goverment" => "Nathalia"
+];
+
+// MENGGANTI SEMUA KEY KE HURUF BESAR
+var_dump(array_change_key_case($SOD,CASE_UPPER));
+echo "\n\n"; 
+// MENGGANTI SEMUA KEY KE HURUF KECIL
+var_dump(array_change_key_case($SOD,CASE_LOWER)); 
+echo "\n\n";
+
+?>
